f61eabcb30842a77cdc4933592a06e7b19e1abb8
Nicholas Wong <100066879@student.swin.edu.au>
2018-05-19 00:32:08 +1000

    Dev-deployment #7

--- scripts/Deploy.sh
+++ scripts/Deploy.sh
@@ -2,10 +2,9 @@
 
 serverless config credentials --provider aws --key $access_key_id --secret $secret_access_key
 
-
-
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
+if [ $TRAVIS_BRANCH == 'master' ]; then
   npm run deploy:stage
-elif [[ $TRAVIS_BRANCH == 'develop' ]]; then
+  npm run deploy:production
+elif [ $TRAVIS_BRANCH == 'develop' ]; then
   npm run deploy
 fi
