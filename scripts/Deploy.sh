dd1c6a994c078d65db5a830ee6db55e05a1e7ef7
Nicholas Wong <100066879@student.swin.edu.au>
2018-05-18 23:46:08 +1000

    Deployment Test #6

--- scripts/Deploy.sh
+++ scripts/Deploy.sh
@@ -6,6 +6,7 @@ serverless config credentials --provider aws --key $access_key_id --secret $secr
 
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
   npm run deploy:stage
+  npm run deploy:production
 elif [[ $TRAVIS_BRANCH == 'develop' ]]; then
   npm run deploy
 fi
