8c30a9d6a75b55445a091c8afce32377caecb6bf
Nicholas Wong <100066879@student.swin.edu.au>
2018-05-18 23:22:49 +1000

    Dev-deployment Test #2

--- scripts/Deploy.sh
+++ scripts/Deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 serverless config credentials --provider aws --key $access_key_id --secret $secret_access_key
 
 
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
   if [[ $TRAVIS_BRANCH == 'master' ]]; then
     npm run deploy
   elif [[ $TRAVIS_BRANCH == 'develop' ]]; then
     npm run deploy:stage
   fi
+fi
