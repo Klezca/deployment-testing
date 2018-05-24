7e76ced1d403b30f79bad93cdd79229e9c7fd56a
Nicholas Wong <100066879@student.swin.edu.au>
2018-05-25 00:11:15 +1000

    Deployment Test #32

--- src/dynamodb/create.js
+++ src/dynamodb/create.js
@@ -11,7 +11,23 @@ exports.create = (event, context, callback) => {
     },
     TableName: process.env.DYNAMODB_TABLE,
   };
-  documentClient.put(params, (err, data) => {
-    callback(err, data);
+  documentClient.put(params, error => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: "Couldn't create the todo item.",
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
   });
 };
