9e5020d1c8308e3df29c5896b281a60c83a8a3a2
Nicholas Wong <100066879@student.swin.edu.au>
2018-05-24 23:48:16 +1000

    Deployment Test #30

--- src/dynamodb/create.js
+++ src/dynamodb/create.js
@@ -1,33 +1,17 @@
 const AWS = require('aws-sdk');
-const uuidv4 = require('uuid/v4');
+const uuidv1 = require('uuid/v4');
 
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
 exports.create = (event, context, callback) => {
   const params = {
     Item: {
-      id: uuidv4(),
+      id: uuidv1(),
       Name: event.name,
     },
     TableName: process.env.DYNAMODB_TABLE,
   };
-  documentClient.put(params, error => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: "Couldn't create the todo item.",
-      });
-      return;
-    }
-
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
+  documentClient.put(params, (err, data) => {
+    callback(err, data);
   });
 };
